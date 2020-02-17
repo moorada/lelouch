542b58fada81998919873fad2dfe2acc8cf5f04b
moorada <a.robustella1@studenti.uniba.it>
2020-02-18 00:27:49 +0100

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lelouch
+## In sviluppo
+Questo tool permetterà di fare pratica con la conversione fonetica (The major system), in lingua italiana. 
--- majorSystem.go
+++ majorSystem.go
@@ -1,1 +1,33 @@
 package main
+
+type Figure struct {
+	char      rune
+	phonetics []string
+}
+
+type MS [10]Figure
+//TODO Risolvere il problema della "gl", a volte si legge palatale, a volte gutturale.
+
+var ItalianMS = MS{
+	{'0', []string{"s", "z", "sc","sc"}},
+	{'1', []string{"t", "d"}},
+	{'2', []string{"n", "gn"}},
+	{'3', []string{"m"}},
+	{'4', []string{"r"}},
+	{'5', []string{"l", "gl"}},
+	{'6', []string{"ce", "ci", "ge", "gi", "j"}},
+	{'7', []string{"c","g","ca", "co", "cu", "ch", "ga", "go", "gu", "gh", "q","k"}},
+	{'8', []string{"f", "v"}},
+	{'9', []string{"p", "b"}},
+}
+
+var Vowels = []rune{'a', 'e', 'i', 'o', 'u', 'à', 'é', 'í', 'ó', 'ú', 'à', 'è', 'ì', 'ò', 'ù'}
+
+func IsVowel(vowel rune) bool {
+	for _, c := range Vowels {
+		if vowel == c {
+			return true
+		}
+	}
+	return false
+}
